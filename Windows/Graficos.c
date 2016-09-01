51ad757351031f0d341b854aa6f9919d6b96a92f
Bernardo Oliveira Bruning <bernardo.bruning@gmail.com>
2016-08-31 23:31:03 -0300

    :construction: Ajusta a biblioteca de graficos

--- Windows/Graficos.c
+++ Windows/Graficos.c
@@ -1,10 +1,22 @@
+//Using SDL and standard IO
+#include "SDL2/SDL.h"
 #include <stdio.h>
 
-void llvm_bibliotecas_Graficos() {
-	printf("Ola Mundo!");
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+void llvm_bibliotecas_Graficos_inicializar() {
+	 //The window we'll be rendering to
+    SDL_Window* window = NULL;
+    
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize!");
+    }
 }
 
-int main( int argc, const char* argv[] )
-{
-	llvm_bibliotecas_Graficos();
-}
