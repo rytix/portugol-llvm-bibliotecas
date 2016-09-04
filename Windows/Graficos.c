857754a80489c7e62f1c45200e3d0bf65b98b815
Bernardo Oliveira Bruning <bernardo.bruning@gmail.com>
2016-09-04 20:25:31 -0300

    :sunglasses: Implementa funções de desenhar... teste desenha linhas

--- Windows/Graficos.c
+++ Windows/Graficos.c
@@ -3,62 +3,85 @@
 #include <SDL.h>
 #include "Graficos.h"
 
+const int DEBUG = 0;
+
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_inicializar() {
-	//The window we'll be rendering to
-    SDL_Window* window = NULL;
-    
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+SDL_Window* window;
+SDL_Renderer *renderer;
 
-    //Initialize SDL
+int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_criar_cor(int r, int g, int b) {
+	if(DEBUG) printf("Criando cor r:%d g:%d b:%d \n", r, g, b);
+    int rgb = r;
+    rgb = (rgb << 8) + g;
+    rgb = (rgb << 8) + b;
+	return rgb;
+}
+
+void portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_definir_cor(int cor) {
+	if(DEBUG) printf("Definindo cor \n", cor);
+    int red = (cor >> 16) & 0xFF;
+    int green = (cor >> 8) & 0xFF;
+    int blue = cor & 0xFF;
+
+    
+    SDL_SetRenderDrawColor(renderer, red, green, blue, 0);
+}
+
+void portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_desenhar_ponto(int x, int y) {
+	if(DEBUG) printf("Desenhando ponto na posição x:%d y:%d \n", x, y);
+    SDL_RenderDrawPoint(renderer, x, y); //Renders on middle of screen.
+    SDL_RenderPresent(renderer);
+    SDL_PumpEvents();
+}
+
+int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_carregar_imagem(char* endereco) {
+    return 0;
+}
+
+void portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_iniciar_modo_grafico(int manter_visivel){
+    if(DEBUG) printf("Iniciando modo grafico...\n");
+
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        printf( "Problemas ao inicializar o modo gráfico: %s\n", SDL_GetError() );
     }
 
      else
     {
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        //Cria janela
+        SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
         if( window == NULL )
         {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            printf( "Janela não foi criada: %s\n", SDL_GetError() );
         }
         else
         {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-
-            //Wait two seconds
-            SDL_Delay( 2000 );
+            //Atualiza tela
+            SDL_UpdateWindowSurface(window);
         }
     }
-
-	return 0;
 }
 
-int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_criar_cor(int r, int g, int b) {
-	printf("Criando cor r:%d g:%d b:%d \n", r, g, b);
-	return 0;
+void portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_encerrar_modo_grafico(){
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
-int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_definir_cor(int cor) {
-	printf("Definindo cor \n", cor);
-	return 0;
+int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_liberar_imagem(int endereco){
+    return 0;
 }
 
-int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_desenhar_ponto(int x, int y) {
-	printf("Desenhando ponto na posição x:%d y:%d \n", x, y);
-	return 0;
+void portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_renderizar(){
+
 }
 
+void portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_renderizar_imagem(int largura, int altura){
+
+}
+
+void portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaGraficos_desenhar_imagem(int x, int y, int endereco){
+
+}
--- test/src/programa.por
+++ test/src/programa.por
@@ -1,7 +1,22 @@
 programa 
 {
+	inclua biblioteca Graficos --> graficos
 	funcao inicio () 
 	{ 
-			escreva("Oi... \n")
+		graficos.iniciar_modo_grafico(falso)
+		inteiro cor = graficos.criar_cor(255, 0, 0)
+		graficos.definir_cor(cor)
+		
+		inteiro tamanho = 300
+
+		para(inteiro i=0; i<tamanho; i++) {
+			para(inteiro j=0; j<tamanho; j++) {
+				graficos.desenhar_ponto(100+i, 100+j)
+				graficos.renderizar()
+			}
+		}
+
+
+		graficos.encerrar_modo_grafico()
 	} 
 }
