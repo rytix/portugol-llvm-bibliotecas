c1d0e92023cc36e1a1a01aab572858ae0e545a05
Bernardo Oliveira Bruning <bernardo.bruning@gmail.com>
2016-09-08 21:46:29 -0300

    :construction: Adiciona projeto para android

---
+++ test/android-project/jni/Application.mk
@@ -0,0 +1,9 @@
+
+# Uncomment this if you're using STL in your project
+# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
+# APP_STL := stlport_static 
+
+NDK_TOOLCHAIN_VERSION := clang
+
+APP_ABI := armeabi armeabi-v7a x86
+APP_STL := stlport_static 
