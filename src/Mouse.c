da3b83e545ad036fee3b486522fcd3fbbfce6630
Bernardo Oliveira Bruning <bernardo.bruning@gmail.com>
2016-09-18 22:16:17 -0300

    :construction: Adiciona arquivo de biblioteca gráfica

---
+++ src/Mouse.c
@@ -0,0 +1,33 @@
+//Using SDL and standard IO
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <SDL.h>
+#if ANDROID
+#include <SDL_image.h>
+#endif
+#if WIN32
+#include <SDL2/SDL_image.h>
+#endif
+#include "Mouse.h"
+
+int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaMouse_posicao_x() {
+    return 0;
+}
+
+int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaMouse_posicao_y() {
+    return 0;
+}
+
+int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaMouse_ler_botao() {
+    return 0;
+}
+
+int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaMouse_botao_pressionado(int botao) {
+    return 0;
+}
+
+int portugol_core_llvm_bibliotecas_portugol_core_llvm_bibliotecas_BibliotecaMouse_algum_botao_pressionado() {
+    return 0;
+}
+
