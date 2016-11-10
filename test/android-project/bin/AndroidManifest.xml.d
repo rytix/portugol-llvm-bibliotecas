b930b0c2fc71841df50e759db193a65d99621bf7
Bernardo Oliveira Bruning <bernardo.bruning@gmail.com>
2016-11-09 23:59:08 -0200

    :construction: adiciona arquivos para serem ignorados

--- test/android-project/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\bin\AndroidManifest.xml : \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\AndroidManifest.xml \
--- test/android-project/bin/SDLActivity-debug-unaligned.apk.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\bin\SDLActivity-debug-unaligned.apk : \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\bin\SDLActivity.ap_ \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\bin\classes.dex \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\libs\armeabi-v7a\libmain.so \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\libs\armeabi-v7a\libportugol.so \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\libs\armeabi-v7a\libSDL2.so \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\libs\armeabi-v7a\libSDL2_image.so \
