57cc4eeae136c63b2212afef8ef30ccd60e0e601
Paulo Eduardo Martins <aff12300@gmail.com>
2018-11-14 16:31:51 -0200

    ...

--- test/android-project/gen/R.java.d
+++ test/android-project/gen/R.java.d
@@ -1,14 +1,14 @@
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\gen\org\libsdl\app\R.java \
- : C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\res\layout\main.xml \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\res\mipmap-hdpi\ic_launcher.png \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\res\mipmap-mdpi\ic_launcher.png \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\res\mipmap-xhdpi\ic_launcher.png \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\res\mipmap-xxhdpi\ic_launcher.png \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\res\mipmap-xxxhdpi\ic_launcher.png \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\res\values\strings.xml \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\bin\res\mipmap-hdpi\ic_launcher.png \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\bin\res\mipmap-mdpi\ic_launcher.png \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\bin\res\mipmap-xhdpi\ic_launcher.png \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\bin\res\mipmap-xxhdpi\ic_launcher.png \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\bin\res\mipmap-xxxhdpi\ic_launcher.png \
-C:\Users\Bernardo\Projects\science-computer\llvm\portugol-llvm-bibliotecas\test\android-project\bin\AndroidManifest.xml \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\gen\org\libsdl\app\R.java \
+ : C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\res\layout\main.xml \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\res\mipmap-hdpi\ic_launcher.png \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\res\mipmap-mdpi\ic_launcher.png \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\res\mipmap-xhdpi\ic_launcher.png \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\res\mipmap-xxhdpi\ic_launcher.png \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\res\mipmap-xxxhdpi\ic_launcher.png \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\res\values\strings.xml \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\bin\res\mipmap-hdpi\ic_launcher.png \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\bin\res\mipmap-mdpi\ic_launcher.png \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\bin\res\mipmap-xhdpi\ic_launcher.png \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\bin\res\mipmap-xxhdpi\ic_launcher.png \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\bin\res\mipmap-xxxhdpi\ic_launcher.png \
+C:\Users\lite\Desktop\portugol-llvm-bibliotecas\test\android-project\bin\AndroidManifest.xml \
